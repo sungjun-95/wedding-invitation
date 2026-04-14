a6df20a019fb38d7487457cbf443d7bf5ed78ad1
sungjun-95 <jeu2250@gmail.com>
2026-04-14 23:21:03 +0900

    Add index.ts for image imports and exports
    Created an index file to import and export images for use in the application.

---
+++ src/images/index.ts
@@ -0,0 +1,42 @@
+import coverImage from "./cover.jpg"
+import image1 from "./image1.jpg"
+import image2 from "./image2.jpg"
+import image3 from "./image3.jpg"
+import image4 from "./image4.jpg"
+import image5 from "./image5.jpg"
+import image6 from "./image6.jpg"
+import image7 from "./image7.jpg"
+import image8 from "./image8.jpg"
+import image9 from "./image9.jpg"
+import image10 from "./image10.jpg"
+import image11 from "./image11.jpg"
+import image12 from "./image12.jpg"
+import image13 from "./image13.jpg"
+import image14 from "./image14.jpg"
+import image15 from "./image15.jpg"
+import image16 from "./image16.jpg"
+import image17 from "./image17.jpg"
+import image18 from "./image18.jpg"
+
+export const COVER_IMAGE = coverImage
+
+export const GALLERY_IMAGES = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18
+]
