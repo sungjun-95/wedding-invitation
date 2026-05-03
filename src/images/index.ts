499285da5c96f0a1d3aa84f869b72c52bb0b3696
sungjun-95 <jeu2250@gmail.com>
2026-05-03 22:49:11 +0900

    Add files via upload

---
+++ src/images/index.ts
@@ -0,0 +1,54 @@
+import coverImage from "./cover.jpg"
+import image1 from "./image1.jpg"
+import image1b from "./image1b.jpg"
+import image2 from "./image2.jpg"
+import image3 from "./image3.jpg"
+import image4 from "./image4.jpg"
+import image5 from "./image5.jpg"
+import image6 from "./image6.jpg"
+import image6a from "./image6a.jpg"
+import image7 from "./image7.jpg"
+import image8 from "./image8.jpg"
+import image9 from "./image9.jpg"
+import image9a from "./image9a.jpg"
+import image9b from "./image9b.jpg"
+import image10 from "./image10.jpg"
+import image11 from "./image11.jpg"
+import image12 from "./image12.jpg"
+import image12a from "./image12a.jpg"
+import image12b from "./image12b.jpg"
+import image13 from "./image13.jpg"
+import image15 from "./image15.jpg"
+import image16 from "./image16.jpg"
+import image17 from "./image17.jpg"
+import image18 from "./image18.jpg"
+import image19 from "./image19.jpg"
+
+export const COVER_IMAGE = coverImage
+
+export const GALLERY_IMAGES = [
+  image1,
+  image1b,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image6a,
+  image7,
+  image8,
+  image9,
+  image9a,
+  image9b,
+  image10,
+  image11,
+  image12,
+  image12a,
+  image12b,
+  image13,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+]
